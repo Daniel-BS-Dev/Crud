8768d5c91443a7c4d4ba3bb229be31202bb320e4
Daniel-BS-Dev <danielbenedito355@gmail.com>
2021-11-07 14:10:54 -0300

    externalizing application

--- backend/src/main/java/com/devsuperior/dscatalog/config/AppConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dscatalog.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -8,16 +9,23 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 public class AppConfig {
+	// criada a variavel para ser usada em ambiente externo
+	@Value("${jwt.secret}")
+	private String jwtSecret;
 
+	
+	// metodo para criptografa a senha do usuario
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 	
+	
+	//adicionando o meus bean para dar acesso a o token pelo email
 	@Bean
 	public JwtAccessTokenConverter accessTokenConverter() {
 		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-		tokenConverter.setSigningKey("jwtSecret");
+		tokenConverter.setSigningKey(jwtSecret);
 		return tokenConverter;
 	}
 
